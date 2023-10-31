fc539777ab7b06955e6e1ede63dc045e0aa05037
OwenGilman <129552649+OwenGilman@users.noreply.github.com>
2023-10-31 16:46:09 -0400

    Update schema.sql
    Changed names of attributes

--- schema.sql
+++ schema.sql
@@ -2,8 +2,8 @@ CREATE TABLE USER (
     username        VARCHAR(15)       PRIMARY KEY,
     fname           VARCHAR(20)       NOT NULL,
     lname           VARCHAR(20)       NOT NULL,
-    date_of_birth   DATE              NOT NULL,
-    pswd            VARCHAR(20)       NOT NULL,    
+    dob             DATE              NOT NULL,
+    password        VARCHAR(20)       NOT NULL,    
 )
 
 
@@ -15,23 +15,23 @@ CREATE TABLE ADMIN (
 
 CREATE TABLE END_USER (
     end_username     VARCHAR(15)       PRIMARY KEY,
-    user_address     VARCHAR(255),
+    end_address     VARCHAR(255),
     FOREIGN KEY(end_username) REFERENCES  USER(username)  
 )
 
 
 CREATE TABLE SELLER (
-    seller_name     VARCHAR(15)       PRIMARY KEY,
-    rating          INT,
-    items_sold      INT,
-    FOREIGN KEY(seller_name) REFERENCES END_USER(username) 
+    seller_username     VARCHAR(15)       PRIMARY KEY,
+    rating              INT,
+    items_sold          INT,
+    FOREIGN KEY(seller_username) REFERENCES END_USER(username) 
 );
 
 
 CREATE TABLE BUYER (
-    buyer_name      VARCHAR(15)       PRIMARY KEY,
-    items_bought    INT,
-    FOREIGN KEY(buyer_name) REFERENCES END_USER(username) 
+    buyer_username      VARCHAR(15)       PRIMARY KEY,
+    items_bought        INT,
+    FOREIGN KEY(buyer_username) REFERENCES END_USER(username) 
 );
 
 
@@ -50,9 +50,9 @@ CREATE TABLE REVIEWS (
     rev_content     VARCHAR(150)      NOT NULL,
     item_id         INT               NOT NULL,
     rating          INT               NOT NULL,
-    buyer_name      VARCHAR(15)       NOT NULL,
+    buyer_username      VARCHAR(15)       NOT NULL,
     FOREIGN KEY(item_id)  REFERENCES  ITEM(iid),      
-    FOREIGN KEY(buyer_name) REFERENCES  BUYER(buyer_name)   
+    FOREIGN KEY(buyer_username) REFERENCES  BUYER(buyer_username)   
 );
 
 
@@ -65,12 +65,12 @@ CREATE TABLE ITEM (
     category        VARCHAR(20)       NOT NULL,
     post_date       TIME              NOT NULL,
     picture         BOOLEAN           NOT NULL,
-    descrp          VARCHAR(150),
-    buyer_name      VARCHAR(15),
-    seller_name     VARCHAR(15),
+    description     VARCHAR(150),
+    buyer_username  VARCHAR(15),      NOT NULL,
+    seller_username VARCHAR(15),      NOT NULL,
     price           DEC(8,2)          NOT NULL,
-    FOREIGN KEY(seller_name)  REFERENCES  END_USER(username),      
-    FOREIGN KEY(buyer_name) REFERENCES  END_USER(username)   
+    FOREIGN KEY(seller_username)  REFERENCES  END_USER(username),      
+    FOREIGN KEY(buyer_username) REFERENCES  END_USER(username)   
 );
 
 
@@ -79,20 +79,20 @@ CREATE TABLE MESSAGES (
     msg_date        TIME              NOT NULL,
     msg_subject     VARCHAR(20),
     msg_content     VARCHAR(1000)     NOT NULL,
-    sender_name     VARCHAR(15)       NOT NULL,
-    recipient_name  VARCHAR(15)       NOT NULL,
-    FOREIGN KEY(sender_id)    REFERENCES USER(username),
-    FOREIGN KEY(recipient_id) REFERENCES USER(username),
+    sender_username VARCHAR(15)       NOT NULL,
+    rec_username    VARCHAR(15)       NOT NULL,
+    FOREIGN KEY(sender_username)    REFERENCES USER(username),
+    FOREIGN KEY(rec_username) REFERENCES USER(username),
 );
 
 
 
 CREATE TABLE BUYS (
     date_bought        TIME              NOT NULL,
-    buyer_name         VARCHAR(15)       NOT NULL,
+    buyer_username     VARCHAR(15)       NOT NULL,
     item_id            INT               NOT NULL,
     quantity           INT               NOT NULL,
-    PRIMARY KEY(buyer_name, item_id, date_bought),
+    PRIMARY KEY(buyer_username, item_id, date_bought),
     FOREIGN KEY(buyer_name)    REFERENCES BUYER(buyer_name),
     FOREIGN KEY(item_id) REFERENCES ITEM(iid),
 );
